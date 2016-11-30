c0cd645ae875fd6ffb7c83da07aa638475570f8a
William Godard <grenier.godard@gmail.com>
2016-11-30 12:00:29 +0100

    Update amadeus requests

--- app/services/amadeus_service.rb
+++ app/services/amadeus_service.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class AmadeusService
   attr_accessor :origin, :destination, :departure_date, :one_way, :duration, :direct, :max_price, :aggregation_mode
 
@@ -13,20 +16,31 @@ class AmadeusService
     @aggregation_mode = options[:aggregation_mode]
   end
 
-  def get_inspiration(options)
-    missing = options.keys - [:apikey, :origin]
-    raise "#{missing.join(", ")} are missing" if missing.size > 1
-    raise "#{missing.join(", ")} is missing" if missing.size > 0
+  def get_inspiration(*options)
     url = 'https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search'
     uri = url + '?' + 'apikey=' + @apikey + '&' + 'origin=' + @origin
+    uri = uri + '&' + 'departure_date=' + @departure_date unless @departure_date.blank?
+    uri = uri + '&' + 'direct=' + @direct.to_s unless @direct.blank?
+    uri = uri + '&' + 'destination=' + @destination unless @destination.blank?
+    puts uri
+    result = open(uri).read
+    result_json = JSON.parse(result)
+    puts result_json
   end
 
-  def get_low_fare(options)
-    missing = options.keys - [:apikey, :origin, :destination, :departure_date]
+  def get_low_fare(options = {})
+    missing = [:destination, :departure_date] - options.keys
     raise "#{missing.join(", ")} are missing" if missing.size > 1
     raise "#{missing.join(", ")} is missing" if missing.size <= 1
     url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search'
     uri = url + '?' + 'apikey=' + @apikey + '&' + 'origin=' + @origin
     uri = uri + '&' + 'destination=' + @destination + 'departure_date=' + @departure_date
+    uri = uri + '&' + 'direct=' + @direct.to_s unless @direct.blank?
+    uri = uri + '&' + 'duration=' + @duration.to_s unless @duration.blank?
+    puts uri
+    result = open(uri).read
+    result_json = JSON.parse(result)
+    puts result_json
   end
+
 end
