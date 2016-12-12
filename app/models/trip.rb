dfa2213dd53e801368132622a96c3800bc856e17
William Godard <grenier.godard@gmail.com>
2016-12-12 19:56:20 +0100

    Add front and trip index controller

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,15 @@ class Trip < ActiveRecord::Base
 
   validates :duration, presence: true
   validates :departure_date, presence: true
+
+  def self.find_cheapest(options)
+    date     = options[:departure_date]
+    origin   = options[:origin]
+    duration = options[:duration]
+    people   = options[:people]
+
+    results  = JSON.parse($redis.get(origin))
+    results  = results[date][duration]['results']
+    results  = results.sort {|a, b| a["price"].to_i + a["airbnb"]["1"].to_i <=> b["price"].to_i + b["airbnb"]["1"].to_i}
+  end
 end
